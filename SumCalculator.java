2e0f542adf3d863dcc72b01f5dd3f4d1587f3c4e
tkdhage <77530981+tkdhage@users.noreply.github.com>
2025-08-12 08:04:34 +0530

    Create SumCalculator.java

---
+++ SumCalculator.java
@@ -0,0 +1,7 @@
+public class SumCalculator {
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        System.out.println("Sum: " + (num1 + num2));
+    }
+}
